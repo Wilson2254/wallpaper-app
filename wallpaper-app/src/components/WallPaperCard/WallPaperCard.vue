67de92a7db29ccbab928d7979cd0327bd1394d6a
Aleksandr.Poteryayko <Sanek191296@mail.ru>
2022-09-12 23:02:34 +0300

    develop - init project

---
+++ wallpaper-app/src/components/WallPaperCard/WallPaperCard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+      <img class="rounded-md" src="https://placehold.jp/1920x900.png" alt="">
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ wallpaper-app/src/modules/main/main.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import WallPaperCard from  '../../components/WallPaperCard/WallPaperCard.vue'
+
+const getWallPaper = () : void => {
+  console.log('Send request')
+}
+
+onMounted(() => {
+  getWallPaper()
+})
+
+</script>
+<template>
+  <div class="p-2 flex gap-x-2">
+    <wall-paper-card class="w-4/12"></wall-paper-card>
+  </div>
+
+</template>
