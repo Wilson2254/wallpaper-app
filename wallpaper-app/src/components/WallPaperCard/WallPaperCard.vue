7d8cc629a40455d1fe23e9cbe2ab14fa286a5e45
Aleksandr.Poteryayko <Sanek191296@mail.ru>
2022-09-18 23:01:18 +0300

    develop - add fake placeholder pictures

--- wallpaper-app/src/components/WallPaperCard/WallPaperCard.vue
+++ wallpaper-app/src/components/WallPaperCard/WallPaperCard.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+interface Props {
+  msg?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  msg: 'hello',
+})
 </script>
 
 <template>
   <div>
+    {{props.msg}}
       <img class="rounded-md" src="https://placehold.jp/1920x900.png" alt="">
   </div>
 </template>
--- wallpaper-app/src/modules/main/main.vue
+++ wallpaper-app/src/modules/main/main.vue
@@ -1,19 +1,45 @@
-<script setup lang="ts">
-import { onMounted } from 'vue'
+<script lang="ts">
+import { ref, defineComponent  } from 'vue'
+import axios from 'axios'
 import WallPaperCard from  '../../components/WallPaperCard/WallPaperCard.vue'
 
-const getWallPaper = () : void => {
-  console.log('Send request')
-}
+export default defineComponent({
+  setup() {
+    let listOfPhotos = ref({})
 
-onMounted(() => {
-  getWallPaper()
+    // expose to template and other options API hooks
+    return {
+      listOfPhotos
+    }
+  },
+
+  mounted() {
+    this.getWallPaper().then(photos => {
+      this.listOfPhotos = photos
+    })
+  },
+
+  methods: {
+    async getWallPaper() : Promise<Object>  {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/albums/1/photos');
+        return response.data;
+      } catch (err) {
+        console.log(err);
+        return {}
+      }
+    }
+  },
+
+  components: {
+    WallPaperCard
+  }
 })
-
 </script>
+
 <template>
-  <div class="p-2 flex gap-x-2">
-    <wall-paper-card class="w-4/12"></wall-paper-card>
+  <div class="p-2 grid grid-cols-3 gap-x-4">
+    <wall-paper-card v-for="photoItem in listOfPhotos" :key="photoItem.id"></wall-paper-card>
   </div>
 
 </template>
