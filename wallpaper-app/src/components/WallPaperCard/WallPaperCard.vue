b095fca9e77b63ad2b8bd7be0505c0bce2b4c129
Aleksandr.Poteryayko <Sanek191296@mail.ru>
2022-10-13 00:13:03 +0400

    develop - change setup api for TS

--- wallpaper-app/src/components/WallPaperCard/WallPaperCard.vue
+++ wallpaper-app/src/components/WallPaperCard/WallPaperCard.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
 interface Props {
-  msg?: string
+  imgSource?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  msg: 'hello',
+  imgSource: '',
 })
 </script>
 
 <template>
   <div>
-    {{props.msg}}
-      <img class="rounded-md" src="https://placehold.jp/1920x900.png" alt="">
+      <img class="rounded-md" :src=props.imgSource alt="">
   </div>
 </template>
 
--- wallpaper-app/src/modules/main/main.vue
+++ wallpaper-app/src/modules/main/main.vue
@@ -1,45 +1,32 @@
-<script lang="ts">
-import { ref, defineComponent  } from 'vue'
+<script setup lang="ts">
+import {ref, onMounted} from 'vue'
 import axios from 'axios'
 import WallPaperCard from  '../../components/WallPaperCard/WallPaperCard.vue'
 
-export default defineComponent({
-  setup() {
-    let listOfPhotos = ref({})
+let listOfPhotos = ref({})
 
-    // expose to template and other options API hooks
-    return {
-      listOfPhotos
-    }
-  },
-
-  mounted() {
-    this.getWallPaper().then(photos => {
-      this.listOfPhotos = photos
-    })
-  },
-
-  methods: {
-    async getWallPaper() : Promise<Object>  {
-      try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/albums/1/photos');
-        return response.data;
-      } catch (err) {
-        console.log(err);
-        return {}
-      }
-    }
-  },
-
-  components: {
-    WallPaperCard
-  }
+onMounted(() => {
+  getWallPaper()
 })
+
+const fetchWallPaper = async () : Promise<Object>  => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/albums/1/photos');
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return {}
+  }
+}
+
+const getWallPaper = async() : Promise<void> => {
+  listOfPhotos.value = await fetchWallPaper()
+}
 </script>
 
 <template>
   <div class="p-2 grid grid-cols-3 gap-x-4">
-    <wall-paper-card v-for="photoItem in listOfPhotos" :key="photoItem.id"></wall-paper-card>
+    <wall-paper-card v-for="photoItem in listOfPhotos" :key="photoItem.id" :img-source="photoItem.url"></wall-paper-card>
   </div>
 
 </template>
